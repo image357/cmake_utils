e0af63f4057e0f3bffc6dcf8b62e60d2e7b6b0c1
Marcel Köpke <marcel.koepke@gmail.com>
2025-07-28 18:14:50 +0200

    Update installConfig.in.cmake

--- installConfig.in.cmake
+++ installConfig.in.cmake
@@ -13,9 +13,7 @@ include("${CMAKE_CURRENT_LIST_DIR}/installTargets.cmake")
 check_required_components("@PROJECT_NAME@")
 
 # status message
-if (NOT DEFINED @PROJECT_NAME@_FOUND)
 set(@PROJECT_NAME@_ROOT "${CMAKE_CURRENT_LIST_DIR}/..")
 cmake_path(NORMAL_PATH @PROJECT_NAME@_ROOT)
 string(REGEX REPLACE "/$" "" @PROJECT_NAME@_ROOT "${@PROJECT_NAME@_ROOT}")
 message(STATUS "Found @PROJECT_NAME@: ${@PROJECT_NAME@_ROOT} (found version \"@PROJECT_VERSION@\")")
-endif ()
