4198eade8437022a77d2bfbb19d8411f52f76422
Marcel Köpke <marcel.koepke@gmail.com>
2025-07-28 17:56:22 +0200

    Update installConfig.in.cmake

--- installConfig.in.cmake
+++ installConfig.in.cmake
@@ -1,8 +1,19 @@
 @PACKAGE_INIT@
 
 include(CMakeFindDependencyMacro)
-# find_dependency(SomeDependency)
+#find_dependency(Boost COMPONENTS system)
 
+# add search path
+if (NOT "${CMAKE_CURRENT_LIST_DIR}" IN_LIST CMAKE_MODULE_PATH)
+list(APPEND CMAKE_MODULE_PATH "${CMAKE_CURRENT_LIST_DIR}")
+endif ()
+
+# include targets
 include("${CMAKE_CURRENT_LIST_DIR}/installTargets.cmake")
 check_required_components("@PROJECT_NAME@")
-message(STATUS "Found @PROJECT_NAME@: ${CMAKE_CURRENT_LIST_FILE} (found version \"@PROJECT_VERSION@\")")
+
+# status message
+set(@PROJECT_NAME@_ROOT "${CMAKE_CURRENT_LIST_DIR}/..")
+cmake_path(NORMAL_PATH @PROJECT_NAME@_ROOT)
+string(REGEX REPLACE "/$" "" @PROJECT_NAME@_ROOT "${@PROJECT_NAME@_ROOT}")
+message(STATUS "Found @PROJECT_NAME@: ${@PROJECT_NAME@_ROOT} (found version \"@PROJECT_VERSION@\")")
